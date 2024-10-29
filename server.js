eb63d524bde3868f9058e2445768f2cf0732fb5e
Edgardo N. Angeles <87631824+Infin8Phy6@users.noreply.github.com>
2024-10-29 22:21:47 +0800

    Initial commit of existing folder

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors()); // Enable CORS
+app.use(bodyParser.json()); // Parse JSON bodies
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
