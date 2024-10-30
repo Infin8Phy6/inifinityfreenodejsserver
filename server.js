59b9a3cf44f985125b47ce4679b3a35c5f790b0a
Edgardo N. Angeles <87631824+Infin8Phy6@users.noreply.github.com>
2024-10-30 10:33:36 +0800

    v18

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3000; // You can change the port if needed
-
+app.use(cors());
 // Endpoint to send JSON data
 app.get('/greet', (req, res) => {
     res.json({ Greetings: 'Hi from the server!' });
